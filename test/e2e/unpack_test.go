41c4d4885876a817a0ec5384ddc2829b5c4bdae8
Bryce Palmer <bpalmer@redhat.com>
2023-06-20 12:52:05 -0400

    fix goimports
    Signed-off-by: Bryce Palmer <bpalmer@redhat.com>

--- test/e2e/unpack_test.go
+++ test/e2e/unpack_test.go
@@ -7,12 +7,13 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	catalogd "github.com/operator-framework/catalogd/api/core/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+
+	catalogd "github.com/operator-framework/catalogd/api/core/v1alpha1"
 )
 
 const (
